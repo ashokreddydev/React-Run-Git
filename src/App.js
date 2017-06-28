08a54e345247badd9daad29209de52a0220a2d27
Ashok Reddy Penamalli <ashokcse505@gmail.com>
2017-06-28 13:03:09 +0530

    Add

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Jumbotron</h1>
+        <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p><a className="btn btn-primary btn-lg">Learn more</a></p>
+    </div>
+    );
+  }
+}
+
+export default App;
